8cf0a43610a96471764b8881c13b90ab4d07bef1
MonikaPandian <panmonikmm@gmail.com>
2022-07-26 12:02:54 +0530

    price-cart-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Free from './Free';
+import Plus from './Plus';
+import Pro from './Pro';
+import './index.css';
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="pricing py-5">
+    <div className="container">
+      <div className="row">
+        <Free />
+        <Plus />
+        <Pro />        
+      </div>
     </div>
-  );
+  </section>
+  )
 }
+
 
 export default App;
