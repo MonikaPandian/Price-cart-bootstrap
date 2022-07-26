204b817175fb5a13b4094771d98166db291e2f04
MonikaPandian <panmonikmm@gmail.com>
2022-07-26 19:17:02 +0530

    price-cart-bootstrap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () =>{
   return (
     <section className="pricing py-5">
     <div className="container">
-      <div className="row">
+      <div className="row pt-4">
         <Free />
         <Plus />
         <Pro />        
